6f643591b9f7fd81e9705abd45c345798dc12e56
Afnan-Yusuf <afnanyusufpp@gmail.com>
2024-07-20 16:32:55 +0530

    measuring weight ready

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
+#include "HX711.h"
 
-// put function declarations here:
-int myFunction(int, int);
+const int LOADCELL_DOUT_PIN = D5; // GPIO 14
+const int LOADCELL_SCK_PIN = D6;  // GPIO 12
+const int BUTTON_PIN = D7;        // GPIO 13
+
+HX711 scale;
+float calibration_factor = 476; 
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.tare(10); 
+  Serial.println("Scale initialized");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    Serial.println("Tare button pressed");
+    scale.tare(); 
+    delay(500); 
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (scale.is_ready()) {
+    float weight = scale.get_units(10); 
+    Serial.print("Weight: ");
+    Serial.print(weight, 2);
+    Serial.println(" kg");
+  } 
 }
