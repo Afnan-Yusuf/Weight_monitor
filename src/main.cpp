623ad9571c82d0650a862016cf398051edba436e
Afnan-Yusuf <afnanyusufpp@gmail.com>
2024-07-21 11:08:54 +0530

    reading and sending via esp now

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
+//A4:CF:12:FF:44:77 MAC address of esp1
+//50:02:91:77:C2:2C MAC address of esp2
+
 #include <Arduino.h>
 #include "HX711.h"
+#include <ESP8266WiFi.h>
+#include <espnow.h>
 
 const int LOADCELL_DOUT_PIN = D5; // GPIO 14
 const int LOADCELL_SCK_PIN = D6;  // GPIO 12
@@ -7,6 +12,12 @@ const int BUTTON_PIN = D7;        // GPIO 13
 
 HX711 scale;
 float calibration_factor = 476; 
+float w = 0;
+
+uint8_t receiverMac[] = {0xA4, 0xCF, 0x12, 0xFF, 0x44, 0x77}; // Replace with your receiver's MAC address
+
+void onSent(uint8_t *macAddr, uint8_t status) {
+}
 
 void setup() {
   Serial.begin(115200);
@@ -15,19 +26,30 @@ void setup() {
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.tare(10); 
   Serial.println("Scale initialized");
+
+
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_register_send_cb(onSent);
+  esp_now_add_peer(receiverMac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
 
 void loop() {
   if (digitalRead(BUTTON_PIN) == LOW) {
     Serial.println("Tare button pressed");
-    scale.tare(); 
+    scale.tare(10); 
     delay(500); 
   }
 
   if (scale.is_ready()) {
     float weight = scale.get_units(10); 
-    Serial.print("Weight: ");
-    Serial.print(weight, 2);
-    Serial.println(" kg");
+    Serial.println(weight);
+    w = weight;
+    
   } 
+   esp_now_send(receiverMac, (uint8_t *)&w, sizeof(w));
 }
